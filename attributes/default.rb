d7dfb5910aafbc90813c42923d69e6ce157bdbc6
= <nagaraju.mymail@gmail.com>
2019-02-01 09:18:44 -0600

    Update key and repo information

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,8 +7,8 @@ default[:ecqmEngine][:ecqmEngine_env_vars] =
 }
 
 default[:ecqmEngine][:js_ecqm_install_path] = '/opt/js-ecqm-engine'
-default[:ecqmEngine][:js_ecqm_repository] = 'https://dl.packager.io/srv/deb/giriraj0209/js-ecqm-engine/master/ubuntu'
-default[:ecqmEngine][:js_ecqm_repository_key] = 'https://dl.packager.io/srv/giriraj0209/js-ecqm-engine/key'
+default[:ecqmEngine][:js_ecqm_repository] = 'https://dl.packager.io/srv/deb/OSEHRA/js-ecqm-engine/master/ubuntu'
+default[:ecqmEngine][:js_ecqm_repository_key] = 'https://dl.packager.io/srv/OSEHRA/js-ecqm-engine/key'
 default[:ecqmEngine][:js_ecqm_version] = ''
 
 force_default['erlang']['install_method'] = "esl"
